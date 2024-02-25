9c0ee710d59d79ea98dbe834953c30282b562d7a
Dzmitry Alikhver <dolihver@gmail.com>
2024-02-25 14:59:46 +0300

    2 modules added

---
+++ song-service/src/main/java/com/example/resourcesongservice/ResourceSongServiceDzmitryAlikhverApplication.java
@@ -0,0 +1,13 @@
+package com.example.resourcesongservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceSongServiceDzmitryAlikhverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceSongServiceDzmitryAlikhverApplication.class, args);
+	}
+
+}
---
+++ song-service/src/test/java/com/example/resourcesongservice/ResourceSongServiceDzmitryAlikhverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourcesongservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceSongServiceDzmitryAlikhverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
